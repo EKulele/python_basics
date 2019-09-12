9721ecf87fa73ded5202c1b32d4ebc97da1b7ed0
e_kulele <markizz71112@gmail.com>
2019-09-12 21:28:22 +0300

    ДЗ1

--- less_01.py
+++ less_01.py
@@ -7,16 +7,16 @@ if age <= 30:
     if weight >= 50 and weight <= 120
         print(name, surname, " - пациент в хорошем состоянии")
     else:
-	      print(name, surname, " - пациенту требуется врачебный осмотр")
+	    print(name, surname, " - пациенту требуется врачебный осмотр")
   
 elif age > 30:
     if weight > 50 and weight < 120:
         print(name, surname, " - пациенту требуется начать вести правильный образ жизни")
     else:
-	      print(name, surname, " - пациенту необходимо пройти полное обследование")	
+        print(name, surname, " - пациенту необходимо пройти полное обследование")	
   
 elif age >= 40:
     if weight > 50 and weight < 120:
         print(name, surname, " - пациенту требуется врачебный осмотр")
     else:
-	      print(name, surname, " - врач удивлен, почему пациент еще жив")
+	    print(name, surname, " - врач удивлен, почему пациент еще жив")
